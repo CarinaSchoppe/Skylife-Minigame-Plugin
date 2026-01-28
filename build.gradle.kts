15e6f7874211b8f4183b81334a32043463ce6b72
Carina Schoppe <info@carinaschoppe.com>
2026-01-28 19:47:20 +1000

    Update Kotlin JVM plugin version to latest in `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "+"
     idea
     id("com.gradleup.shadow") version "9.2.2"
     id("xyz.jpenilla.run-paper") version "3.0.2"
